72d93d520ff4bd9a43fa501d0805555529a6a205
Maciej M <22326971+madziejm@users.noreply.github.com>
2019-06-27 12:21:06 +0200

    Rename variables and called methods identifier

--- cymbaline/generators.py
+++ cymbaline/generators.py
@@ -14,17 +14,17 @@ class Generator:
     def generate(self, context_words: list) -> str:
         raise NotImplementedError("Abstract `Generator` base class method has been called.")
 
-    def _generate_n_syllables_verse(self, n: int, context_words: list) -> str:
+    def _generate_n_syllable_verse(self, n: int, context_words: list) -> str:
         verse = ""
 
         while n > 0:
-            word_syllables_count = random.randint(1, n)
+            word_syllable_count = random.randint(1, n)
             # non-empty verse conditional
             if verse:
                 verse += " "
-            candidate_words = self.lt.n_syllables_top_related_words(syllables_count=word_syllables_count, context_words=self.context_words)
+            candidate_words = self.lt.n_syllable_top_related_words(syllable_count=word_syllable_count, context_words=self.context_words)
             verse += random.choice(candidate_words)
-            n -= word_syllables_count
+            n -= word_syllable_count
         
         return verse
     
@@ -32,10 +32,10 @@ class Generator:
 class HaikuGenerator(Generator):
 
     def generate(self, context_words: list) -> str:
-        haiku = self._generate_n_syllables_verse(5, context_words)
+        haiku = self._generate_n_syllable_verse(5, context_words)
         haiku += '\n'
-        haiku += self._generate_n_syllables_verse(7, context_words)
+        haiku += self._generate_n_syllable_verse(7, context_words)
         haiku += '\n'
-        haiku += self._generate_n_syllables_verse(5, context_words)
+        haiku += self._generate_n_syllable_verse(5, context_words)
         
         return haiku
