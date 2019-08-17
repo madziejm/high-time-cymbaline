1694fb82da5f70be292d1a03c35f93183e629662
Maciej M <22326971+madziejm@users.noreply.github.com>
2019-08-17 09:02:08 +0200

    Add non-bogus deployment option

--- cymbaline/__init__.py
+++ cymbaline/__init__.py
@@ -4,4 +4,10 @@ from cymbaline.interactions import Interaction
 
 class Bot:
     def deploy(self, target: str = "Production", debug: bool = False):
-        pass
+        print(f"Deploying the bot on {target}")
+        if target == "CLI":
+            while True:
+                user_input = input("> ")
+                interaction = Interaction(user_input=user_input)
+                bot_response = interaction.get_response()
+                print(bot_response)
